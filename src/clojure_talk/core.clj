31248322adb006648489b9e9de0b68ad1ef041a4
rod <rod@pu-gh.com>
2012-02-08 00:03:49 +0000

    added basic project with cheshire

---
+++ src/clojure_talk/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-talk.core)
---
+++ test/clojure_talk/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-talk.test.core
+  (:use [clojure-talk.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
