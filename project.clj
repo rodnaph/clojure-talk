0def5305b92590b7a08ffa28ce82188617a180ba
rod <rod@pu-gh.com>
2012-02-29 17:11:04 +0000

    stuff

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
                  [compojure "1.0.1"]
                  [ring "1.0.2"]
                  [ring-serve "0.1.2"]
+                 [hiccup "0.3.8"]
                  [cheshire "2.2.0"]])
 
--- src/clojure_talk/core.clj
+++ src/clojure_talk/core.clj
@@ -1,12 +1,24 @@
 
 (ns clojure-talk.core
     (:use compojure.core
-          ring.middleware.reload))
+          ring.middleware.params
+          ring.middleware.reload
+          [hiccup core form-helpers]))
+
+(defn show-results-page
+    [{:keys [params]}]
+    "Results")
+
+(defn show-search-page
+    [{:keys [params]}]
+    (html [:form
 
 (defroutes main-routes
-    (GET "/" [] "Hello, Unified Diff!"))
+    (GET "/" [] show-search-page)
+    (GET "/:q" [] show-results-page))
 
 (def app 
     (-> main-routes
+        (wrap-params)
         (wrap-reload 'clojure-talk.core)))
 
