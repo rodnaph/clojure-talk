866f7d61d079eb836562965e9069a774affc55b2
rod <rod@pu-gh.com>
2012-03-06 16:11:02 +0000

    added sequences example

---
+++ src/clojure_talk/sequences.clj
@@ -0,0 +1,30 @@
+
+(ns clojure-talk.sequences)
+
+(def numbers '[1 1 2 3 5 8 13])
+
+; // double numbers
+; var $numbers = [ 1, 1, 2, 3, 5, 8, 13 ];
+; for ( var $i=0; i<$numbers.length; $i++ ) {
+;   $numbers[ i ] = $numbers[ i ] * 2;
+; }
+
+
+; // then triple the numbers?
+
+
+; // work out some kind of total
+; var total = 0;
+; foreach ( $number in $numbers ) {
+;   $total += $number;
+; }
+
+; // filter to a subset of data
+; var $evenNumbers = array();
+; foreach ( $number in $numbers ) {
+;   if ( $number % 2 ) {
+;     $evenNumbers.push( $number );
+;   }
+; }
+
+
